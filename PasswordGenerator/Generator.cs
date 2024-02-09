3471153980e9d3f68ddc218b871175f7d5531c92
albertar95 <alb.rajabi@gmail.com>
2024-02-10 00:19:43 +0330

    home

--- PasswordGenerator/Generator.cs
+++ PasswordGenerator/Generator.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +21,7 @@ namespace PasswordGenerator
         private List<string> specials = new List<string>() { ".","#", "$", "!", "%", "&", "@", "?", "=", ";", ":", "*", "~", "|", "_", "+",
             "^", ",","-", "{", "}", "(", ")", "[", "]", "<", ">" };
         private List<string> test = new List<string>() { "a","b","c" };
+        public string DestinationFilePath { get; set; } = ConfigurationManager.AppSettings["DestinationFilePath"];
         public Generator(int Length, List<CharType> charTypes)
         {
             _Length = Length;
@@ -26,27 +29,29 @@ namespace PasswordGenerator
         }
         public IEnumerable<string> GeneratePassword()
         {
-            double rowCount = GetRowCount();//8
+            double rowCount = GetRowCount();//9
             int rowLength = rowCount > int.MaxValue ? int.MaxValue : Convert.ToInt32(rowCount);//9
+
             string[,] passwordMatrix = new string[rowLength, _Length];//9,2
+
             string[] correspondingChars = GetChars().ToArray();//a,b,c
             var corresCount = correspondingChars.Count();//3
+
             int corresIndex = 0;
-            for (int i = 0; i < _Length; i++)
+            prepareChunckFiles();
+            for (int i = _Length - 1; i >= 0; i--)
             {
-                for (int k = _Length - 1; k >= 0; k--)
+                for (int j = 0; j < rowLength / Math.Pow(corresCount, i); j++)
                 {
-                    for (int j = 0; j < rowLength / Math.Pow(corresCount, k); j++)
+                    var period = Convert.ToInt32(Math.Pow(corresCount, i));
+                    for (int x = 0; x < period; x++)
                     {
-                        var period = Convert.ToInt32(Math.Pow(corresCount, k));
-                        for (int x = 0; x < period; x++)
-                        {
-                            passwordMatrix[(j * period) + x, i] = correspondingChars[corresIndex];
-                        }
-                        corresIndex++;
-                        if (corresIndex >= corresCount)
-                            corresIndex = 0;
+                        //passwordMatrix[(j * period) + x, i] = correspondingChars[corresIndex];
+                        WriteInTempFile(correspondingChars[corresIndex],i);
                     }
+                    corresIndex++;
+                    if (corresIndex >= corresCount)
+                        corresIndex = 0;
                 }
             }
             return MatrixToRow(passwordMatrix);
@@ -66,7 +71,7 @@ namespace PasswordGenerator
             }
             return result;
         }
-        private double GetRowCount()
+        public double GetRowCount()
         {
             double rowCount = 0;
             foreach (var ct in _CharTypes)
@@ -135,6 +140,55 @@ namespace PasswordGenerator
                 }
             }
             return result;
+        }
+        public int GetCharsCount()
+        {
+            int rowCount = 0;
+            foreach (var ct in _CharTypes)
+            {
+                if (_CharTypes.Contains(CharType.all))
+                {
+                    rowCount = numbers.Count + lowercases.Count + uppercases.Count + specials.Count;
+                    break;
+                }
+                else
+                {
+                    switch (ct)
+                    {
+                        case CharType.numbers:
+                            rowCount += numbers.Count;
+                            break;
+                        case CharType.lowercaseCharacters:
+                            rowCount += lowercases.Count;
+                            break;
+                        case CharType.uppercaseCharacters:
+                            rowCount += uppercases.Count;
+                            break;
+                        case CharType.specials:
+                            rowCount += specials.Count;
+                            break;
+                        case CharType.test:
+                            rowCount += test.Count;
+                            break;
+                    }
+                }
+            }
+            return rowCount;
+        }
+        private void prepareChunckFiles()
+        {
+            for (int i = 0; i < _Length; i++)
+            {
+                var path = Path.Combine(DestinationFilePath, $"chunk{i}.txt");
+                if (File.Exists(path))
+                    File.Delete(path);
+                File.Create(path);
+                File.SetAttributes(path, File.GetAttributes(path) | FileAttributes.Hidden);
+            }
+        }
+        private void WriteInTempFile(string corresChar,int fileId)
+        {
+            File.AppendAllLines(Path.Combine(DestinationFilePath, $"chunk{fileId}.txt"), new List<string>() { corresChar });
         }
     }
     public enum CharType
--- PasswordGenerator/Program.cs
+++ PasswordGenerator/Program.cs
@@ -12,9 +12,155 @@ namespace PasswordGenerator
         {
             Console.WriteLine("enter password length");
             var len = int.Parse(Console.ReadLine());
-            var generator = new Generator(len,new List<CharType>() {  CharType.test });
+            var generator = new Generator(len,new List<CharType>() {  CharType.numbers });
             var result = generator.GeneratePassword();
             Console.ReadKey();
+        }
+        static void GatherInputs()
+        {
+            Console.WriteLine("==================== Password Generator ====================");
+            PasswordLength:
+            Console.WriteLine("enter password length");
+            int passwordLength = 0;
+            int charachtersOption = 0;
+            if (!int.TryParse(Console.ReadLine(), out passwordLength))
+            {
+                Console.WriteLine("incorrect input");
+                goto PasswordLength;
+            }
+            correspondingCharacters:
+            Console.WriteLine("choose corresponding charachters" + Environment.NewLine);
+            Console.WriteLine("1.All");
+            Console.WriteLine("2.numbers");
+            Console.WriteLine("3.lowercase charachters");
+            Console.WriteLine("4.uppercase charachters");
+            Console.WriteLine("5.special charachters");
+            Console.WriteLine("6.numbers + lowercase charachters");
+            Console.WriteLine("7.numbers + uppercase charachters");
+            Console.WriteLine("8.numbers + special charachters");
+            Console.WriteLine("9.lowercase charachters + uppercase charachters");
+            Console.WriteLine("10.lowercase charachters + special charachters");
+            Console.WriteLine("11.uppercase charachters + special charachters");
+            Console.WriteLine("12.numbers + lowercase charachters + uppercase charachters");
+            Console.WriteLine("13.numbers + uppercase charachters + special charachters");
+            Console.WriteLine("14.numbers + lowercase charachters + special charachters");
+            Console.WriteLine("15.lowercase charachters + uppercase charachters + special charachters");
+            Console.WriteLine(Environment.NewLine);
+            if (!int.TryParse(Console.ReadLine(), out charachtersOption))
+            {
+                Console.WriteLine("incorrect input");
+                goto correspondingCharacters;
+            }
+            List<CharType> correspondings = new List<CharType>();
+            switch (charachtersOption)
+            {
+                case 1:
+                    correspondings.Add(CharType.all);
+                    break;
+                case 2:
+                    correspondings.Add(CharType.numbers);
+                    break;
+                case 3:
+                    correspondings.Add(CharType.lowercaseCharacters);
+                    break;
+                case 4:
+                    correspondings.Add(CharType.uppercaseCharacters);
+                    break;
+                case 5:
+                    correspondings.Add(CharType.specials);
+                    break;
+                case 6:
+                    correspondings.Add(CharType.numbers);
+                    correspondings.Add(CharType.lowercaseCharacters);
+                    break;
+                case 7:
+                    correspondings.Add(CharType.numbers);
+                    correspondings.Add(CharType.uppercaseCharacters);
+                    break;
+                case 8:
+                    correspondings.Add(CharType.numbers);
+                    correspondings.Add(CharType.specials);
+                    break;
+                case 9:
+                    correspondings.Add(CharType.lowercaseCharacters);
+                    correspondings.Add(CharType.uppercaseCharacters);
+                    break;
+                case 10:
+                    correspondings.Add(CharType.lowercaseCharacters);
+                    correspondings.Add(CharType.specials);
+                    break;
+                case 11:
+                    correspondings.Add(CharType.uppercaseCharacters);
+                    correspondings.Add(CharType.specials);
+                    break;
+                case 12:
+                    correspondings.Add(CharType.numbers);
+                    correspondings.Add(CharType.lowercaseCharacters);
+                    correspondings.Add(CharType.uppercaseCharacters);
+                    break;
+                case 13:
+                    correspondings.Add(CharType.numbers);
+                    correspondings.Add(CharType.uppercaseCharacters);
+                    correspondings.Add(CharType.specials);
+                    break;
+                case 14:
+                    correspondings.Add(CharType.lowercaseCharacters);
+                    correspondings.Add(CharType.numbers);
+                    correspondings.Add(CharType.specials);
+                    break;
+                case 15:
+                    correspondings.Add(CharType.lowercaseCharacters);
+                    correspondings.Add(CharType.uppercaseCharacters);
+                    correspondings.Add(CharType.specials);
+                    break;
+            }
+            ProcessInputs(passwordLength,correspondings);
+            GatherInputs();
+        }
+        static void ProcessInputs(int length,List<CharType> chars)
+        {
+            Generator gen = new Generator(length,chars);
+            var rowCount = gen.GetRowCount();
+            var correspondingCount = gen.GetCharsCount();
+            Console.WriteLine(Environment.NewLine + "----------------------------------------" + Environment.NewLine);
+            Console.WriteLine($"you have choose {length} as password length and {correspondingCount} characters for generating passwords.");
+            Console.WriteLine($"total count of passwords = {rowCount}");
+            if(rowCount >= 2147483647)
+            {
+                Console.WriteLine("total count of password is too big and its not possible to continue");
+                return;
+            }
+            Console.WriteLine($"generated password file size = {CalculateFileSize(rowCount,correspondingCount)}" + Environment.NewLine);
+            fire:
+            Console.WriteLine("do you want to start generating passwords? (y/n)");
+            var fire = Console.ReadLine();
+            switch (fire.ToLower())
+            {
+                case "y":
+                    break;
+                case "n":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("incorrect input");
+                    goto fire;
+            }
+
+
+        }
+        static string CalculateFileSize(double rowCount,int CorresCount)
+        {
+            var totalChars = rowCount * CorresCount;
+            if (totalChars < 1024)
+                return "1 kb";
+            else if (totalChars > 1024 && totalChars < 1048576)//kb
+                return $"{totalChars / 1024} kb";
+            else if (totalChars > 1048576 && totalChars < 1073741824)//mb
+                return $"{totalChars / 1048576} mb";
+            else if (totalChars > 1073741824 && totalChars < 966367641600)//gb
+                return $"{totalChars / 1073741824} gb";
+            else
+                return "more that 900 gb!";
         }
     }
 }
